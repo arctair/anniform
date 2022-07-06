835eed07d7c64afd2b1a254f49efe319c2326378
tyler <tyler@cruftbusters.com>
2022-07-06 12:53:33 -0700

    add dummy stone annihilation and formation nodes

--- source/data.lua
+++ source/data.lua
@@ -13,3 +13,31 @@ stoneChestRecipe.ingredients = {{"stone", 10}}
 stoneChestRecipe.result = "stone-chest"
 
 data:extend{stoneChestEntity, stoneChestItem, stoneChestRecipe}
+
+local annihilationNodeEntity = table.deepcopy(data.raw["container"]["stone-chest"])
+annihilationNodeEntity.name = "stone-annihilation-node"
+
+local annihilationNodeItem = table.deepcopy(data.raw["item"]["stone-chest"])
+annihilationNodeItem.name = "stone-annihilation-node"
+annihilationNodeItem.place_result = "stone-annihilation-node"
+
+local annihilationNodeRecipe = table.deepcopy(data.raw["recipe"]["stone-chest"])
+annihilationNodeRecipe.enabled = true
+annihilationNodeRecipe.name = "stone-annihilation-node"
+annihilationNodeRecipe.result = "stone-annihilation-node"
+
+data:extend{ annihilationNodeEntity, annihilationNodeItem, annihilationNodeRecipe }
+
+local formationNodeEntity = table.deepcopy(data.raw["container"]["stone-chest"])
+formationNodeEntity.name = "stone-formation-node"
+
+local formationNodeItem = table.deepcopy(data.raw["item"]["stone-chest"])
+formationNodeItem.name = "stone-formation-node"
+formationNodeItem.place_result = "stone-formation-node"
+
+local formationNodeRecipe = table.deepcopy(data.raw["recipe"]["stone-chest"])
+formationNodeRecipe.enabled = true
+formationNodeRecipe.name = "stone-formation-node"
+formationNodeRecipe.result = "stone-formation-node"
+
+data:extend{ formationNodeEntity, formationNodeItem, formationNodeRecipe }
