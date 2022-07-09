be007fb9686b430497a11e36843269b5f654bcc6
tyler <tyler@cruftbusters.com>
2022-07-08 19:35:15 -0700

    log when any stone annihilation node was destroyed

--- source/control.lua
+++ source/control.lua
@@ -1,16 +1,20 @@
+script.on_init(function()
+    global.stone_annihilation_node_registration_numbers = {}
+end)
+
 function on_stone_annihilation_node_built(event)
     log('stone annihilation node was built')
-    global.last_built_registration_number = script.register_on_entity_destroyed(event.created_entity)
+    global.stone_annihilation_node_registration_numbers[script.register_on_entity_destroyed(event.created_entity)] = true
 end
 
 function on_entity_destroyed(event)
-    if global.last_built_registration_number and event.registration_number == global.last_built_registration_number then
-        on_last_built_stone_annihilation_node_destroyed(event)
+    if global.stone_annihilation_node_registration_numbers[event.registration_number] ~= nil then
+        on_stone_annihilation_node_destroyed(event)
     end
 end
 
-function on_last_built_stone_annihilation_node_destroyed(event)
-    log('last built stone annihilation node was destroyed')
+function on_stone_annihilation_node_destroyed(event)
+    log('stone annihilation node was destroyed')
 end
 
 script.on_event(
