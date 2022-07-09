1cb7de63fe7c263a6709036d6b37f9c00059857e
tyler <tyler@cruftbusters.com>
2022-07-08 17:53:38 -0700

    log when stone annihilation node is built

--- source/control.lua
+++ source/control.lua
@@ -1,1 +1,3 @@
-log("hello world")
+script.on_event(defines.events.on_built_entity, function(event)
+    log(event.item.name .. ' was built')
+end, {{ filter="name",name="stone-annihilation-node" }})
