dedd6de738aa36d80f0def767921640f9641b657
tyler <tyler@cruftbusters.com>
2022-07-08 23:38:07 -0700

    add basic graphics to differentiate anniform game objects from wooden chest

--- source/data.lua
+++ source/data.lua
@@ -19,10 +19,14 @@ data:extend { stoneChestEntity, stoneChestItem, stoneChestRecipe }
 local annihilationNodeEntity = table.deepcopy(data.raw["container"]["stone-chest"])
 annihilationNodeEntity.name = "stone-annihilation-node"
 annihilationNodeEntity.minable.result = "stone-annihilation-node"
+annihilationNodeEntity.icon = "__anniform__/graphics/icons/stone-annihilation-node.png"
+annihilationNodeEntity.picture.layers[1].filename = "__anniform__/graphics/entity/stone-annihilation-node.png"
+annihilationNodeEntity.picture.layers[1].hr_version = nil
 
 local annihilationNodeItem = table.deepcopy(data.raw["item"]["stone-chest"])
 annihilationNodeItem.name = "stone-annihilation-node"
 annihilationNodeItem.place_result = "stone-annihilation-node"
+annihilationNodeItem.icon = "__anniform__/graphics/icons/stone-annihilation-node.png"
 
 local annihilationNodeRecipe = table.deepcopy(data.raw["recipe"]["stone-chest"])
 annihilationNodeRecipe.enabled = true
@@ -34,10 +38,14 @@ data:extend { annihilationNodeEntity, annihilationNodeItem, annihilationNodeReci
 local formationNodeEntity = table.deepcopy(data.raw["container"]["stone-chest"])
 formationNodeEntity.name = "stone-formation-node"
 formationNodeEntity.minable.result = "stone-formation-node"
+formationNodeEntity.icon = "__anniform__/graphics/icons/stone-formation-node.png"
+formationNodeEntity.picture.layers[1].filename = "__anniform__/graphics/entity/stone-formation-node.png"
+formationNodeEntity.picture.layers[1].hr_version = nil
 
 local formationNodeItem = table.deepcopy(data.raw["item"]["stone-chest"])
 formationNodeItem.name = "stone-formation-node"
 formationNodeItem.place_result = "stone-formation-node"
+formationNodeItem.icon = "__anniform__/graphics/icons/stone-formation-node.png"
 
 local formationNodeRecipe = table.deepcopy(data.raw["recipe"]["stone-chest"])
 formationNodeRecipe.enabled = true
@@ -49,10 +57,14 @@ data:extend { formationNodeEntity, formationNodeItem, formationNodeRecipe }
 local transportNodeEntity = table.deepcopy(data.raw["container"]["stone-chest"])
 transportNodeEntity.name = "stone-transport-node"
 transportNodeEntity.minable.result = "stone-transport-node"
+transportNodeEntity.icon = "__anniform__/graphics/icons/stone-transport-node.png"
+transportNodeEntity.picture.layers[1].filename = "__anniform__/graphics/entity/stone-transport-node.png"
+transportNodeEntity.picture.layers[1].hr_version = nil
 
 local transportNodeItem = table.deepcopy(data.raw["item"]["stone-chest"])
 transportNodeItem.name = "stone-transport-node"
 transportNodeItem.place_result = "stone-transport-node"
+transportNodeItem.icon = "__anniform__/graphics/icons/stone-transport-node.png"
 
 local transportNodeRecipe = table.deepcopy(data.raw["recipe"]["stone-chest"])
 transportNodeRecipe.enabled = true
